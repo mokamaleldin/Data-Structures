defa241954180b8576f4dbfb251e612b584909b8
mohamedkeliwa <mhmdalknj496@gmail.com>
2024-11-20 07:54:34 +0300

    Refactor Stack class to use a fixed-size array; simplify push and pop methods

--- Stack/main.cpp
+++ Stack/main.cpp
@@ -1,73 +1,54 @@
 #include <iostream>
 using namespace std;
 
-class Stack {
-private:
-    int* stackData;   // Dynamic array for storing elements
-    int capacity;     // Maximum capacity of the stack
-    int top;          // Index of the top element in the stack
+const int MAX_SIZE = 100;
 
-    // Check if the stack is empty
-    bool isEmpty() {
-        return top == -1;
-    }
+class stack{
+    private:
+        int top;
+        int item[MAX_SIZE];
 
-    // Check if the stack is full
-    bool isFull() {
-        return top + 1 == capacity;
-    }
-
-public:
-    // Constructor with a fixed capacity
-    Stack(int maxCapacity) : capacity(maxCapacity), top(-1) {
-        stackData = new int[capacity];
-    }
-
-    // Destructor to free up memory
-    ~Stack() {
-        delete[] stackData;
-    }
-
-    // Push an element onto the stack
-    void push(int value) {
-        if (isFull()) {
-            cout << "Stack overflow. Cannot push " << value << ".\n";
-            return;
+        bool isFull(){
+            return top == MAX_SIZE - 1;
         }
-        stackData[++top] = value;
-        cout << "Pushed " << value << " onto the stack.\n";
-    }
-
-    // Pop an element from the stack
-    void pop() {
-        if (isEmpty()) {
-            cout << "Stack is empty. Cannot pop.\n";
-            return;
+        bool isEmpty(){
+            return top == -1; 
         }
-        cout << "Popped " << stackData[top--] << " from the stack.\n";
-    }
-
-    // Peek at the top element of the stack
-    int peek() {
-        if (isEmpty()) {
-            cout << "Stack is empty. Cannot peek.\n";
-            return -1; // Return -1 or any other error value
+    public:
+    
+        // stack():top(-1){}
+        stack()
+        {
+            top = -1;
         }
-        return stackData[top];
-    }
+
+       void push(int element){
+            if(isFull()){
+                cout << "Stack is full";
+                return;
+            }
+           top++;
+           item[top] = element;
+       }
+
+       void pop(){
+              if(isEmpty()){
+                cout << "Stack is empty";
+                return;
+              }
+              cout << "This is the element " << item[top]<<endl;
+              top--;
+       }
 };
 
 int main() {
-    Stack s(3); // Create a stack with a maximum capacity of 3
-    s.push(10);
-    s.push(20);
-    cout << "Top element is: " << s.peek() << endl; // Should display 20
-    s.push(30);
-    s.push(40); // Should indicate stack overflow
-    s.pop();
-    cout << "Top element after pop is: " << s.peek() << endl; // Should display 20
-    s.pop();
-    s.pop();
-    s.pop(); // Should indicate stack is empty
+    stack stack1;
+    stack1.push(5);
+    stack1.push(15);
+    stack1.push(2);
+    stack1.pop();
+    stack1.pop();
+
+    cout << "\n\n\n\n\n";
     return 0;
 }
